deffa0a6582257f94d9f473991e74f00a17c701b
carolribeiro2112 <65139655+carolribeiro2112@users.noreply.github.com>
2020-06-24 18:59:19 -0300

    get,post,put and like

--- src/app.js
+++ src/app.js
@@ -30,7 +30,25 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex<0){
+    return response.status(400).json({error:'repository not found'});
+  }
+
+  const repository = {
+    title,
+    url,
+    techs
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
